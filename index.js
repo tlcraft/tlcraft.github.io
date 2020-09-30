7aa03ba08630b293e87f4bd85a7abd9b28be876d
Travis Craft <tlcraft@users.noreply.github.com>
2020-09-30 17:51:34 -0600

    Remove element when animation completes.

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ function generateCopyright() {
 function startTextScroll() {
     const div = document.createElement("div");
     div.setAttribute("class", "text-scroll");
-    div.setAttribute("style", "left: 80%");
+    div.setAttribute("style", "left: 80%; animation-duration: 10s;");
 
     const text = document.createTextNode("Hello World...");
     div.appendChild(text);
     
     const container = document.getElementById("scroll-container");
     container.appendChild(div);
+
+    $(".text-scroll").one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() { 
+        this.remove();
+    });
 }

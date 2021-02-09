e5ee82ee04f9b1c51557c6a61abbcb9452e6aa17
Travis Craft <tlcraft@users.noreply.github.com>
2021-02-08 17:09:58 -0700

    Dynamically reset player puck.

--- index.js
+++ index.js
@@ -310,8 +310,8 @@ function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
     score = 0;
-    x = getRandomIntNonZero(330) + PLAYER_PUCK_RADIUS;
-    y = getRandomIntNonZero(180) + PLAYER_PUCK_RADIUS;
+    x = getRandomIntNonZero(canvas.width - (2 * PLAYER_PUCK_RADIUS)) + PLAYER_PUCK_RADIUS;
+    y = getRandomIntNonZero(canvas.height - (2 * PLAYER_PUCK_RADIUS)) + PLAYER_PUCK_RADIUS;
     xVector = getRandomIntNonZero(4) + 1;
     yVector = getRandomIntNonZero(4) + 1;
     target = null;

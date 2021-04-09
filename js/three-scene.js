59cc33e0b0f3e29511b7c9036205614bacdbff8f
Travis Craft <tlcraft@users.noreply.github.com>
2021-04-09 17:08:10 -0600

    Add method to three-scene.

--- js/three-scene.js
+++ js/three-scene.js
@@ -1,3 +1,5 @@
+import * as THREE from './lib/three/build/three.module.js';
+
 export function toggleThreeScene() {
     const button = document.getElementById('animation-toggle');
     const three = document.getElementById('three');
@@ -7,4 +9,32 @@ export function toggleThreeScene() {
     } else {
         three.style.display = 'none';
     }
+}
+
+export function createThreeScene() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 200, 200 );
+    const threeDiv = document.getElementById('three');
+    threeDiv.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    const animate = function () {
+        requestAnimationFrame( animate );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render( scene, camera );
+    };
+
+    animate();
 }

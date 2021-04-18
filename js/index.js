4b70b5dcc8443b8231767204d3f4c488044f4bab
Travis Craft <tlcraft@users.noreply.github.com>
2021-04-18 14:14:54 -0600

    Add toggle animation method.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { toggleTvPower, leftButtonPress, rightButtonPress, toggleGameAnimation } from './game.js';
 import { startAnimationInterval, toggleTextAnimation } from './text-animation.js';
-import { toggleThreeScene, createThreeScene } from './three-scene.js';
+import { createThreeScene } from './three-scene.js';
 
 $(document).ready(function () {
     initialize();
@@ -11,13 +11,30 @@ $(document).ready(function () {
 
 function initialize() {
     window.toggleTvPower = toggleTvPower;
-    window.toggleTextAnimation = toggleTextAnimation;
     window.leftButtonPress = leftButtonPress;
     window.rightButtonPress = rightButtonPress;
-    window.toggleGameAnimation = toggleGameAnimation;
-    window.toggleThreeScene = toggleThreeScene;
+    window.toggleAnimation = toggleAnimation;
 }
 
 function generateCopyright() {
     $('footer #copyrightDate').text(new Date().getFullYear());
+}
+
+function toggleAnimation() {
+    const button = document.getElementById('animation-toggle');
+    const creations = document.getElementById('creations');
+    const wasAnimationRunning = button.innerHTML === 'Turn Animation Off';
+
+    if (wasAnimationRunning) {
+        creations.style.display = 'none';
+        button.innerHTML = 'Turn Animation On';
+        button.setAttribute('aria-pressed', true);
+    } else {
+        creations.style.display = 'block';
+        button.innerHTML = 'Turn Animation Off';            
+        button.setAttribute('aria-pressed', false);
+    }
+
+    toggleGameAnimation(wasAnimationRunning);
+    toggleTextAnimation(wasAnimationRunning);
 }

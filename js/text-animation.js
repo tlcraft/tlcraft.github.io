f51b57713383c2196cac7d7adfee403772d0da8c
Travis Craft <tlcraft@users.noreply.github.com>
2021-04-20 13:12:12 -0600

    Fix animation check.

--- js/text-animation.js
+++ js/text-animation.js
@@ -20,7 +20,7 @@ export function startAnimationInterval() {
 }
 
 export function toggleTextAnimation(isAnimationRunning) {
-    if (isAnimationRunning) {
+    if (!isAnimationRunning) {
         clearInterval(animationInterval);
         currentAnimationCount = 0;
 

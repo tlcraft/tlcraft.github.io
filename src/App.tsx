58963fc36e35d2c23a5a6b18ae940a26d00511f2
Travis Craft <tlcraft@users.noreply.github.com>
2025-06-20 11:42:46 -0600

    Add boilerplate react site.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello World!</h1>
+      <p>My name is Travis Craft and I'm a full stack software engineer. My experience has revolved around .NET and AWS using agile practices (such as Scrum, SAFe, and Kanban).</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

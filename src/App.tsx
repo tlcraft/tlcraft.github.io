ac19f4c45d95a224168d03719edbd73d18ed9fba
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-09 11:51:21 -0600

    Add profiles section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBluesky, faGithub, faStackOverflow, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
@@ -86,10 +88,17 @@ function App() {
                   </ul>
               </div>
           </div>
+          <p className="profiles">
+              <a href="https://github.com/tlcraft" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="https://bsky.app/profile/travislcraft.bsky.social" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faBluesky} /></a>
+              <a href="https://www.linkedin.com/in/travislcraft/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+              <a href="https://twitter.com/@travislcraft" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+              <a href="https://stackoverflow.com/users/8094831/green-maru" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faStackOverflow} /></a>
+          </p>
         </div>
         <footer>
             <hr/>
-            <a href="https://travislcraft.com">travislcraft</a>
+            <a href="https://travislcraft.com">travislcraft.com</a>
             <h3 id="copyright" className="copyright">Copyright &copy; 2016-{getCopyrightYear()}</h3>
         </footer>
       </section>

fc10f2ed934c4e6a4803091c70493846367c1867
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-14 13:12:48 -0600

    Add technologies component.

---
+++ src/components/technologies/technologies.tsx
@@ -0,0 +1,77 @@
+function Technologies() {
+  return (
+    <div className="technologies">
+        <h2>Technical experience</h2>
+        <div className="skills">
+            <div className="card">
+                <h3>AWS</h3>
+                <ul className="list-disc">
+                    <li>Lambda</li>
+                    <li>Step Functions</li>
+                    <li>DynamoDB</li>
+                    <li>DocumentDB</li>
+                    <li>Textract</li>
+                    <li>PostgreSQL</li>   
+                    <li>SQS</li>
+                    <li>CDK</li>
+                    <li>CLI</li>
+                </ul>
+            </div>
+            <div className="card">
+                <h3>Microsoft</h3>
+                <ul className="list-disc">
+                    <li>TypeScript</li>
+                    <li>ASP.NET Web API and MVC</li>
+                    <li>SQL Server</li>
+                    <li>Entity Framework</li>
+                    <li>C#</li>
+                    <li>Moq</li>
+                    <li>AutoMapper</li>
+                    <li>NuGet</li>
+                    <li>TFS</li>
+                </ul>
+            </div>
+            <div className="card">
+                <h3>Other Technologies</h3>
+                <ul className="list-disc">
+                    <li>Python</li>
+                    <li>Node.js</li>
+                    <li>Angular 2+</li>
+                    <li>Jest / Jasmine</li>
+                    <li>jQuery</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Less / Sass</li>
+                    <li>npm</li>
+                    <li>RESTful APIs</li>
+                    <li>GraphQL</li>
+                    <li>Microservices</li>
+                    <li>Serverless</li>
+                    <li>Terraform</li>
+                </ul>
+            </div>
+            <div className="card">
+                <h3>Tools</h3>
+                <ul className="list-disc">
+                    <li>VS Code</li>
+                    <li>Visual Studio</li>
+                    <li>SQL Server Management Studio</li>
+                    <li>DBeaver</li>
+                    <li>TablePlus</li>
+                    <li>NoSQL Workbench</li>
+                    <li>Postman</li>
+                    <li>Insomnia</li>
+                    <li>git</li>
+                    <li>Jira / Confluence</li>
+                    <li>GitLab / GitHub</li>
+                    <li>Bitbucket</li>
+                    <li>Beyond Compare</li>
+                    <li>Datadog</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Technologies;

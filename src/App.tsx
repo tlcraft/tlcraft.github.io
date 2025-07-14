74320b1ca91e4cf4e61ba0af5430852715b96f00
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-14 13:13:13 -0600

    Use technologies component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBluesky, faGithub, faStackOverflow, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Technologies from './components/technologies/technologies';
 
 function App() {
   return (
@@ -17,77 +18,7 @@ function App() {
           </p>
           <p>Apart from software I practice Aikido and study Japanese using Duolingo and have a recurring language exchange with a friend in Tokyo.</p>
         </div>
-        <div className="technologies">
-          <h2>Technical experience</h2>
-          <div className="skills">
-              <div className="card">
-                  <h3>AWS</h3>
-                  <ul className="list-disc">
-                      <li>Lambda</li>
-                      <li>Step Functions</li>
-                      <li>DynamoDB</li>
-                      <li>DocumentDB</li>
-                      <li>Textract</li>
-                      <li>PostgreSQL</li>   
-                      <li>SQS</li>
-                      <li>CDK</li>
-                      <li>CLI</li>
-                  </ul>
-              </div>
-              <div className="card">
-                  <h3>Microsoft</h3>
-                  <ul className="list-disc">
-                      <li>TypeScript</li>
-                      <li>ASP.NET Web API and MVC</li>
-                      <li>SQL Server</li>
-                      <li>Entity Framework</li>
-                      <li>C#</li>
-                      <li>Moq</li>
-                      <li>AutoMapper</li>
-                      <li>NuGet</li>
-                      <li>TFS</li>
-                  </ul>
-              </div>
-              <div className="card">
-                  <h3>Other Technologies</h3>
-                  <ul className="list-disc">
-                      <li>Python</li>
-                      <li>Node.js</li>
-                      <li>Angular 2+</li>
-                      <li>Jest / Jasmine</li>
-                      <li>jQuery</li>
-                      <li>HTML</li>
-                      <li>CSS</li>
-                      <li>Less / Sass</li>
-                      <li>npm</li>
-                      <li>RESTful APIs</li>
-                      <li>GraphQL</li>
-                      <li>Microservices</li>
-                      <li>Serverless</li>
-                      <li>Terraform</li>
-                  </ul>
-              </div>
-              <div className="card">
-                  <h3>Tools</h3>
-                  <ul className="list-disc">
-                      <li>VS Code</li>
-                      <li>Visual Studio</li>
-                      <li>SQL Server Management Studio</li>
-                      <li>DBeaver</li>
-                      <li>TablePlus</li>
-                      <li>NoSQL Workbench</li>
-                      <li>Postman</li>
-                      <li>Insomnia</li>
-                      <li>git</li>
-                      <li>Jira / Confluence</li>
-                      <li>GitLab / GitHub</li>
-                      <li>Bitbucket</li>
-                      <li>Beyond Compare</li>
-                      <li>Datadog</li>
-                  </ul>
-              </div>
-          </div>
-        </div>
+        <Technologies />
         <footer>
             <hr/>
             <p className="profiles">

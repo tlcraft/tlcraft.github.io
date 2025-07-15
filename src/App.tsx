ab96373911011436a203e3fb99dc19a7ba6e6aab
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-15 16:05:49 -0600

    Use footer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBluesky, faGithub, faStackOverflow, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Footer from './components/footer/footer';
 import Technologies from './components/technologies/technologies';
 
 function App() {
@@ -19,26 +18,11 @@ function App() {
           <p>Apart from software I practice Aikido and study Japanese using Duolingo and have a recurring language exchange with a friend in Tokyo.</p>
         </div>
         <Technologies />
-        <footer>
-            <hr/>
-            <p className="profiles">
-                <a href="https://github.com/tlcraft" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                <a href="https://bsky.app/profile/travislcraft.bsky.social" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faBluesky} /></a>
-                <a href="https://www.linkedin.com/in/travislcraft/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-                <a href="https://twitter.com/@travislcraft" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
-                <a href="https://stackoverflow.com/users/8094831/green-maru" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faStackOverflow} /></a>
-            </p>
-            <a href="https://travislcraft.com">travislcraft.com</a>
-            <h3 id="copyright" className="copyright">Copyright &copy; 2016-{getCopyrightYear()}</h3>
-        </footer>
+        <Footer />
       </section>
       <section></section>
     </div>
   );
-}
-
-function getCopyrightYear(): number {
-  return new Date().getFullYear();
 }
 
 export default App;

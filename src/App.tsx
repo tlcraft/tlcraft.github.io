8b3957bd85cc878cba819a82e169aae49b824431
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-16 10:39:02 -0600

    Use about component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/about/about';
 import Footer from './components/footer/footer';
 import Technologies from './components/technologies/technologies';
 
@@ -7,16 +8,7 @@ function App() {
     <div className="app grid grid-cols-3 gap-4">
       <section></section>
       <section>
-        <div className="hero">
-          <h1>Hello World!</h1>
-          <p>My name is Travis Craft and I'm a full stack software engineer. My experience has revolved around .NET and AWS using agile practices (such as Scrum, SAFe, and Kanban).</p>
-          <p>I like using SOLID principles and functional concepts to write clean code, and I learn a little more about software engineering each day as I work toward Microsoft and AWS certifications.</p>
-          <p>
-              I run a software engineering Meetup group in my community. We hold presentations, lightning talks and have an ongoing book club. We also have monthly lunch meetings.
-              Check out our next meeting here: <a href="https://www.meetup.com/Kalispell-Software-Crafters/">Kalispell Software Crafters</a>.
-          </p>
-          <p>Apart from software I practice Aikido and study Japanese using Duolingo and have a recurring language exchange with a friend in Tokyo.</p>
-        </div>
+        <About />
         <Technologies />
         <Footer />
       </section>

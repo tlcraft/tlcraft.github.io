fd60143ae03f338b5add9f73468c785ae564832d
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-24 12:38:38 -0600

    Fix lint issue.

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="hero">
     <h1>Hello World!</h1>
-    <p>My name is Travis Craft and I'm a full stack software engineer. My experience has revolved around .NET and AWS using agile practices (such as Scrum, SAFe, and Kanban).</p>
+    <p>My name is Travis Craft and I&apos;m a full stack software engineer. My experience has revolved around .NET and AWS using agile practices (such as Scrum, SAFe, and Kanban).</p>
     <p>I like using SOLID principles and functional concepts to write clean code, and I learn a little more about software engineering each day as I work toward Microsoft and AWS certifications.</p>
     <p>
         I run a software engineering Meetup group in my community. We hold presentations, lightning talks and have an ongoing book club. We also have monthly lunch meetings.

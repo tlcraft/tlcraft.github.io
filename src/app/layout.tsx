a745d965f0f7d73d61da9907eecc4dd8932947e0
Travis Craft <tlcraft@users.noreply.github.com>
2025-07-24 12:38:29 -0600

    Update project for nextjs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
-import '../index.css';
-import type { Metadata } from 'next';
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Travis L Craft - About Me',
-    description: 'Web site created with Next.js.',
+  title: 'Travis L Craft - About Me',
+  description: 'Web site created with Next.js.',
 };
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-        <html lang="en">
-          <head>
-            <link rel="icon" href="/favicon.png" />
-            <meta name="theme-color" content="#000000" />
-          </head>
-          <body>
-            <div id="root">{children}</div>
-          </body>
-        </html>           
-    )
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
+      <body
+        className='antialiased'
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

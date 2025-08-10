ccfe7f0fbdd1d977016abb5ba0fcb4974975ece3
Travis Craft <tlcraft@users.noreply.github.com>
2025-08-09 19:34:29 -0600

    Extract params.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,19 @@ import "./page.css";
 import About from "@/components/about/about";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
+import Marquee, { MarqueeProps } from "@/components/marquee/marquee";
 import Technologies from "@/components/technologies/technologies";
+import { faHtml5, faReact, faAngular, faAws, faPython, faCss3, faTrello, IconDefinition } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
+  const technologies: IconDefinition[] = [
+    faHtml5, faReact, faAngular, faAws, faPython, faCss3, faTrello
+  ];
+
+  const marqueeProps: MarqueeProps = {
+    items: technologies,
+  };
+
   return (
     <div className="app grid grid-cols-3 gap-4">
       <section></section>
@@ -12,6 +22,7 @@ export default function Home() {
         <Header />
         <About />
         <Technologies />
+        <Marquee {...marqueeProps} />
         <Footer />
       </section>
       <section></section>
--- src/components/marquee/marquee.tsx
+++ src/components/marquee/marquee.tsx
@@ -1,28 +1,34 @@
-import { faHtml5, faReact, faAngular, faAws, faPython, faCss3, faTrello } from '@fortawesome/free-brands-svg-icons';
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+export interface MarqueeProps {
+    items: IconDefinition[];
+}
+
 // Reference: https://codepen.io/olavih/pen/rNWGPda
-function Marquee() {
+function Marquee(marqueeProps: MarqueeProps) {
+    const { items } = marqueeProps;
+
     return (
         <article className="overflow-hidden whitespace-nowrap w-full flex">
             <div className="relative">
-                <ul className="marquee flex list-none pl-0 text-center">
-                    <li className="p-5"><FontAwesomeIcon icon={faHtml5} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faCss3} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faReact} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faAngular} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faAws} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faPython} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faTrello} /></li>
+                <ul className={ `marquee flex list-none pl-0 text-center` }>
+                    { 
+                        items.map((item, index) => (
+                            <li key={index} className="p-5">
+                                <FontAwesomeIcon icon={item} />
+                            </li>
+                        ))
+                    }
                 </ul>
-                <ul className="marquee2 absolute top-0 flex list-none pl-0 text-center" aria-hidden="true">
-                    <li className="p-5"><FontAwesomeIcon icon={faHtml5} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faCss3} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faReact} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faAngular} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faAws} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faPython} /></li>
-                    <li className="p-5"><FontAwesomeIcon icon={faTrello} /></li>
+                <ul className={`marquee2 absolute top-0 flex list-none pl-0 text-center`} aria-hidden="true">
+                    { 
+                        items.map((item, index) => (
+                            <li key={index} className="p-5">
+                                <FontAwesomeIcon icon={item} />
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </article>

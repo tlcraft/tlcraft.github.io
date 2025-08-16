13891baca85ff2d0b7a324f7ba182e262d900104
Travis Craft <tlcraft@users.noreply.github.com>
2025-08-15 19:02:47 -0600

    Move marquee.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,9 @@ import "./page.css";
 import About from "@/components/about/about";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
-import Marquee, { MarqueeProps } from "@/components/marquee/marquee";
 import Technologies from "@/components/technologies/technologies";
-import { faHtml5, faReact, faAngular, faAws, faPython, faCss3, faTrello, IconDefinition, faJira, faGithub, faGitlab, faBitbucket, faNpm, faLess, faSass, faBootstrap, faNode, faMicrosoft, faAtlassian, faConfluence, faGitAlt, faJs, faJenkins } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
-  const technologies: IconDefinition[] = [
-    faHtml5, faReact, faAngular, faPython, faCss3, faLess, faSass, faBootstrap, faNode, faJs
-  ];
-
-  const tools: IconDefinition[] = [
-    faAws, faTrello, faGitAlt, faJira, faGithub, faGitlab, faBitbucket, faNpm, faMicrosoft, faConfluence, faAtlassian, faJenkins
-  ];
-
-  const technologyMarqueeProps: MarqueeProps = {
-    items: technologies,
-    leftToRight: false,
-  };
-
-  const toolMarqueeProps: MarqueeProps = {
-    items: tools,
-    leftToRight: true,
-  };
-
   return (
     <div className="app grid grid-cols-3 gap-4">
       <section></section>
@@ -32,8 +12,6 @@ export default function Home() {
         <Header />
         <About />
         <Technologies />
-        <Marquee {...technologyMarqueeProps} />
-        <Marquee {...toolMarqueeProps} />
         <Footer />
       </section>
       <section></section>

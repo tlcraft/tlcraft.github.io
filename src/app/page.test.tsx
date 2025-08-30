7bdbf7c4d203ae96d152b10f4b995d3893faeae5
Travis Craft <tlcraft@users.noreply.github.com>
2025-08-30 15:26:46 -0600

    Add tests.

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -7,4 +7,14 @@ describe("PageTests", () => {
       render(<Page />);
       expect(screen.getByRole('img', { name: "Travis Craft" })).toBeDefined();
     });
+
+    it('should include a link to the About section in the header', () => {
+      render(<Page />);
+      expect(screen.getByRole('link', { name: "About" })).toBeDefined();
+    });
+
+    it('should include a link to the Technologies section in the header', () => {
+      render(<Page />);
+      expect(screen.getByRole('link', { name: "Technologies" })).toBeDefined();
+    });
 });

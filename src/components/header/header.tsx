1bcba0c3fcb73c562a1a01ff9578559cfe1466c1
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-02 07:36:34 -0600

    Use Image component.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 function Header() {
     return (
         <div className="inline-flex items-center justify-between">
-            <img src="/travis.jpg" alt="Travis Craft" className="w-[20%] rounded-[50%]" />
+            <Image src="/travis.jpg" alt="Travis Craft" className="w-[20%] rounded-[50%]" />
             <a href='#about' className="text-2xl font-bold m-2">About</a>
             <a href='#technologies' className="text-2xl font-bold m-2">Technologies</a>
         </div>

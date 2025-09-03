11fba9649e84e0468aec1f6a180f5dcd5ebdb9a0
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-03 12:09:47 -0600

    Update header styles.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 function Header() {
     return (
-        <div className="inline-flex items-center justify-between">
-            <Image src="/travis.jpg" alt="Travis Craft" className="w-[20%] rounded-[50%]" />
+        <div className="flex items-center justify-between">
+            <Image src="/travis.jpg" alt="Travis Craft" width="100" height="100" className="rounded-[50%]" />
             <a href='#about' className="text-2xl font-bold m-2">About</a>
             <a href='#technologies' className="text-2xl font-bold m-2">Technologies</a>
         </div>

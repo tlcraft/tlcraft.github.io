2c71164f3da17b2465061d3c91a376a71f8d71e1
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-22 14:00:34 -0600

    Add initial meetup section.

---
+++ src/components/meetup/meetup.tsx
@@ -0,0 +1,21 @@
+import LayoutContainer from "@/components/theme/LayoutContainer";
+
+function Meetup() {
+  return (
+    <section id="meetup">
+      <LayoutContainer className="py-8 lg:py-12">
+        <div>
+          <h2 className="text-2xl font-semibold tracking-tight">Meetup</h2>
+          <p>
+              In the Fall of 2017 I happened to learn about the SOLID principles and Clean Code at a meetup in San Franscico. I was looking for ways to grow as an engineer and this was just what I was looking for. I started lunch and learn meetings at work twice a week and then decided to start a meetup group as well.  
+          </p>
+          <p>
+              Check out what we're doing next here: <a href="https://www.meetup.com/Kalispell-Software-Crafters/">Kalispell Software Crafters</a>.
+          </p>
+        </div>
+      </LayoutContainer>
+    </section>
+  );
+}
+
+export default Meetup;

84c05ae4f6b6514f8f0f4599e97c127201e40c35
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-25 17:26:29 -0600

    Adjusting link styles.

--- src/components/meetup/meetup.tsx
+++ src/components/meetup/meetup.tsx
@@ -2,7 +2,7 @@ import LayoutContainer from "@/components/theme/LayoutContainer";
 
 function Meetup() {
   return (
-    <section id="meetup">
+    <section id="meetup" className="w-full bg-[#D20014] text-white">
       <LayoutContainer className="py-8 lg:py-12">
         <div>
           <h2 className="text-2xl font-semibold tracking-tight">Meetup</h2>
@@ -10,7 +10,7 @@ function Meetup() {
               In the Fall of 2017 I happened to learn about the SOLID principles and Clean Code at a meetup in San Franscico. I was looking for ways to grow as an engineer and this was just what I was looking for. I started lunch and learn meetings at work twice a week and then decided to start a meetup group as well.  
           </p>
           <p>
-              Check out what we're doing next here: <a href="https://www.meetup.com/Kalispell-Software-Crafters/">Kalispell Software Crafters</a>.
+              Check out what we're doing next here: <a className="transition-colors hover:!text-[#008ac5]" href="https://www.meetup.com/Kalispell-Software-Crafters/">Kalispell Software Crafters</a>.
           </p>
         </div>
       </LayoutContainer>

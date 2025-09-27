7512b37414abcd78c1bad00f076f9affee3e01da
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-27 05:46:12 -0600

    Add meetup to header.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,6 +8,7 @@ function Header() {
                 <Image src="/travis.jpg" alt="Travis Craft" width="100" height="100" className="rounded-[50%]" />
                 <a href='#about' className="text-2xl font-bold m-2">About</a>
                 <a href='#technologies' className="text-2xl font-bold m-2">Technologies</a>
+                <a href='#meetup' className="text-2xl font-bold m-2">Meetup</a>
             </div>
         </LayoutContainer>
     )

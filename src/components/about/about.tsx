f2d6890f56ea733b6c45c8432dd27af570302de6
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-28 05:25:57 -0600

    Remove link.

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -10,7 +10,6 @@ function About() {
           <p>I like using SOLID principles and functional concepts to write clean code, and I learn a little more about software engineering each day as I work toward Microsoft and AWS certifications.</p>
           <p>
               I run a software engineering Meetup group in my community. We hold presentations, lightning talks and have an ongoing book club. We also have monthly lunch meetings.
-              Check out our next meeting here: <a href="https://www.meetup.com/Kalispell-Software-Crafters/">Kalispell Software Crafters</a>.
           </p>
           <p>Apart from software I practice Aikido and study Japanese using Duolingo and have a recurring language exchange with a friend in Tokyo.</p>
         </div>

c1b5cd64ec590ba79e9cc4cc91a7fb75c0373bdf
Travis Craft <tlcraft@users.noreply.github.com>
2025-09-29 05:23:49 -0600

    Swap to nav.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,12 +4,12 @@ import LayoutContainer from "@/components/theme/LayoutContainer";
 function Header() {
     return (
         <LayoutContainer>
-            <div className="flex items-center justify-between py-4">
+            <nav className="flex items-center justify-between py-4">
                 <Image src="/travis.jpg" alt="Travis Craft" width="100" height="100" className="rounded-[50%]" />
                 <a href='#about' className="text-2xl font-bold m-2">About</a>
                 <a href='#technologies' className="text-2xl font-bold m-2">Technologies</a>
                 <a href='#meetup' className="text-2xl font-bold m-2">Meetup</a>
-            </div>
+            </nav>
         </LayoutContainer>
     )
 }

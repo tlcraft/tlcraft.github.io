ec9338ee596623554f60c872b11e2fa405a4e289
Travis Craft <tlcraft@users.noreply.github.com>
2025-10-01 18:00:09 -0600

    Add target blank.

--- src/components/meetup/meetup.tsx
+++ src/components/meetup/meetup.tsx
@@ -10,7 +10,7 @@ function Meetup() {
               In the Fall of 2017 I happened to learn about the SOLID principles and Clean Code at a meetup in San Franscico. I was looking for ways to grow as an engineer and this was just what I was looking for. I started lunch and learn meetings at work twice a week and then decided to start a meetup group as well.  
           </p>
           <p>
-              Check out what we're doing next here: <a className="transition-colors hover:!text-[#008ac5]" href="https://www.meetup.com/Kalispell-Software-Crafters/">Kalispell Software Crafters</a>.
+              Check out what we're doing next here: <a className="transition-colors hover:!text-[#008ac5]" href="https://www.meetup.com/Kalispell-Software-Crafters/" target="_blank">Kalispell Software Crafters</a>.
           </p>
         </div>
       </LayoutContainer>

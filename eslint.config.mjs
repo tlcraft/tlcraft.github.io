e3c0ea7903f8e49f5702755ef3fd4e1e8d230a00
Travis Craft <tlcraft@users.noreply.github.com>
2025-10-25 16:56:41 -0600

    Update eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextPlugin from '@next/eslint-plugin-next'
 
-const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "old/**", "coverage/**", "next-env.d.ts"]
-}];
-
+const eslintConfig = defineConfig([
+  nextPlugin.configs['core-web-vitals'],
+  globalIgnores(['node_modules/**', '.next/**', 'out/**', 'old/**', 'coverage/**', 'next-env.d.ts']),
+  { 
+    'rules': {
+        'quotes': ['error', 'single']
+    }
+  }
+]);
+ 
 export default eslintConfig;
